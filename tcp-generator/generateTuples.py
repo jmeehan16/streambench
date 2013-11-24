8f17454caaadc58fb9cdfcdd818ec49e623c1606
Jeff Rasley <jeffra45@gmail.com>
2013-11-24 22:29:39 +0000

    simple program to generate random sentences

---
+++ tcp-generator/generateTuples.py
@@ -0,0 +1,32 @@
+"""
+Tuple: 'TS1384902645 ID12345 this is a sentence'
+
+This program only generates the 'sentence' portion of the tuple
+"""
+import random as r
+import argparse
+
+WORD_COUNT_MIN = 10
+
+def main(args):
+    fd = open(args.input, 'r')
+    sentences = fd.readlines()
+    total_sentences = len(sentences)
+    fdt = open(args.output, 'w')
+
+    for i in xrange(args.samples): 
+        curr_sentence = ""
+        while len(curr_sentence.split(" ")) < WORD_COUNT_MIN:
+            idx = r.randint(1,total_sentences)
+            curr_sentence = sentences[idx] #includes a '\n'
+        fdt.write(curr_sentence)
+
+    fdt.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Generate random sentence tuples from a dataset.')
+    parser.add_argument('-s','--samples', help='Number of samples to write out', required=True, type=int)
+    parser.add_argument('-o','--output', help='Output file name tuples', required=True)
+    parser.add_argument('-i','--input', help='Input file name for sentence dataset', required=False, default='warandpeace.txt')
+    args = parser.parse_args()
+    main(args)
