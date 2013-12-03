196e69dae77b9ba68e135142f7e67a49bf8e0589
jeffra <jeffra45@gmail.com>
2013-12-03 18:38:48 -0500

    assume different defaults

--- tcp-generator/tupleGeneratorServerTime.py
+++ tcp-generator/tupleGeneratorServerTime.py
@@ -7,16 +7,13 @@ import socket
 import argparse
 import time
 
-TCP_IP='localhost'
-TCP_PORT=9999
-
 sent_tuples = []
 
 def main(args):
     fd = open(args.input, 'r')
     dataset = fd.readlines()
-    wait_time_sec = float(args.wait_time) / 1000.0
-    print "Sending tuples for a total of %d seconds" % args.test_time
+    wait_time_sec = args.wait_time / 1000.0
+    print "Sending tuples for a total of %d seconds, with a wait time of %0.4f ms" % (args.test_time, args.wait_time)
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((args.server_host, args.server_port))
@@ -53,9 +50,9 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Read in a tuple data file and send them to a SDBMS')
     parser.add_argument('-i','--input', help='Input file name for tuple data', required=True)
-    parser.add_argument('-s','--server_host', help='Address of SDMBS to send tuples to', required=False, default='localhost')
+    parser.add_argument('-s','--server_host', help='Address of SDMBS to send tuples to', required=False, default='euc03')
     parser.add_argument('-p','--server_port', help='Port of SDMBS to send tuples to', required=False, default=9999, type=int)
-    parser.add_argument('-w','--wait_time', help='The time in milliseconds to sleep between each tuple', required=True, type=int)
+    parser.add_argument('-w','--wait_time', help='The time in milliseconds to sleep between each tuple', required=True, type=float)
     parser.add_argument('-t','--test_time', help='The total time in seconds for the test to run', required=True, type=int, default=30)
     args = parser.parse_args()
  
