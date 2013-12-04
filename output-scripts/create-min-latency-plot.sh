a4294a8926126562829906ff1af847074d632049
jeffra <jeffra45@gmail.com>
2013-12-03 20:53:38 -0500

    more updates to improve context of graphs

--- output-scripts/create-min-latency-plot.sh
+++ output-scripts/create-min-latency-plot.sh
@@ -3,7 +3,9 @@ echo "Summary results"
 python calc-throughput.py catcher_data.txt | grep -A 10 "\-\-"
 python calc-throughput.py catcher_data.txt | grep "  LATENCY:" | sed 's/  LATENCY: //g' > min_latency.dat
 
-export wait_time=$1
+export wait_time=`grep "SENTINEL" catcher_data.txt | cut -d' ' -f2`
+export test_time=`grep "SENTINEL" catcher_data.txt | cut -d' ' -f3`
+
 gnuplot plot_min_latency.gp
 echo "-------------------------"
 echo "created min_latency.png"
--- output-scripts/plot_min_latency.gp
+++ output-scripts/plot_min_latency.gp
@@ -1,9 +1,10 @@
 set terminal pngcairo size 800,600 enhanced font 'Verdana,10'
 set output 'min_latency.png'
 
-#wait_time=system("echo $wait_time")
+wait_time=system("echo $wait_time")
+test_time=system("echo $test_time")
 
-set title "Network Word Count Min Latency, wait-time=" #wait_time
+set title "Network Word Count Min Latency\n wait-time=".wait_time.", test-time=".test_time
 set ylabel "ms"
 set xlabel "Time"
 unset key
