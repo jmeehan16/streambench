027a5e15e96acebe037e2afc04a28332bfec36b2
jeffra <jeffra@cs.brown.edu>
2013-12-19 00:05:22 -0500

    new figure

--- paper/plots/data/extend_tputs.py
+++ paper/plots/data/extend_tputs.py
@@ -1,9 +1,9 @@
 
-fd = open('sent_tputs_sp1.txt', 'r')
+fd = open('sent_tputs_sb1.txt', 'r')
 lines = fd.readlines()
 fd.close()
 
-fd = open('sent_tputs_sp1_extend.txt', 'w')
+fd = open('sent_tputs_sb1_extend.txt', 'w')
 for line in lines:
     for i in range(10):
         fd.write(line)
